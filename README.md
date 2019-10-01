9de07322211e62f46bff3c2538a41772273e019f
David Beckman <david.andrew.beckman@gmail.com>
2019-10-01 17:36:32 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# War
+This is the simple game of War as inspired by the coding kata <https://github.com/gigasquid/wonderland-clojure-katas/tree/master/card-game-war>.
---
+++ src/War.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="N/A">
+//     Copyright © 2019 David Beckman. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace War.Console
+{
+    using Console = System.Console;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
